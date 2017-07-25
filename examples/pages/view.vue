ce93b963e3c061a9df2a4eed3d34182d13c02d5a
yinxueyuan <838476901@qq.com>
2017-07-25 15:59:58 +0800

    fix: replace what a fuck picture

--- examples/pages/view.vue
+++ examples/pages/view.vue
@@ -7,7 +7,7 @@
     export default {
       data () {
         return {
-          imgSrc: 'https://fengyuanchen.github.io/viewerjs/images/thumbnails/tibet-1.jpg',
+          imgSrc: 'https://cdn.leoao.com/o_1blil75gs1p2u1kpm5pp1ov51rfq8.png',
           defaultImg: 'https://cdn.leoao.com/m/img/blank_class.png?imageView2/2/w/200'
         }
       },
--- src/components/view/src/view.vue
+++ src/components/view/src/view.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="le-view" :class="shape ? 'le-view-' + shape : ''" :style="`width: ${realWidth}px; height: ${realHeight}px`">
+  <div class="le-view" :class="shape ? 'le-view-' + shape : ''" :style="`width: ${realWidth}; height: ${realHeight}`">
     <div v-if="defaultImg && defaultShow" class="le-view-default" :class="{'le-view-hide': ready}" :style="`backgroundImage: url(${defaultImg})`"></div>
     <img v-show="ready" class="le-view-img" :class="[{'le-view-show': fade && ready}, {'le-view-cover': cover}]" :src="src" @load="handleImgReady"/>
   </div>
@@ -65,8 +65,8 @@ export default {
   },
 
   mounted: function () {
-    this.realWidth = typeof(this.width) === number ? this.width + 'px' : this.width
-    this.realHeight = typeof(this.height) === number ? this.height + 'px' : this.height
+    this.realWidth = typeof(this.width) === 'number' ? this.width + 'px' : this.width
+    this.realHeight = typeof(this.height) === 'number' ? this.height + 'px' : this.height
   }
 };
 </script>
