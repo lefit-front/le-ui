ecc1d26b94002945e248b7ca5b164a1c6a25bb74
yinxueyuan <838476901@qq.com>
2017-08-28 18:07:28 +0800

    style: rate color

--- src/components/rate/src/rate.vue
+++ src/components/rate/src/rate.vue
@@ -6,7 +6,7 @@
     :class="{'is-active':iValue > i}"
     :style="{color: colors && colors[i-1] ? colors[i-1] : '#ccc',marginRight:margin+'px',fontSize: fontSize + 'px', width: fontSize + 'px', height: fontSize + 'px', lineHeight: fontSize + 'px'}">
       <span class="le-rater-inner">
-        <Icon :type="star" :style="{fontSize: fontSize + 'px', lineHeight: fontSize + 'px'}"/>
+        <Icon :type="star" :style="{fontSize: fontSize + 'px', lineHeight: fontSize + 'px'}" color="#f2f2f2"/>
         <span class="le-rater-outer"
         :style="{color: activeColor, width: cutPercent + '%'}"
         v-if="cutPercent > 0 && cutIndex+1 === i">
