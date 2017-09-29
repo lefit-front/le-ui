5cd3adcfe43c66ce96f5123879b2e3f365c1f48b
yinxueyuan <838476901@qq.com>
2017-09-29 11:21:00 +0800

    fix: view component don't have image url, use default

--- examples/pages/view.vue
+++ examples/pages/view.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="demo-view">
-    <le-view :src="imgSrc || defaultImg" :defaultImg="defaultImg" :width="200" :height="200" shape="round" :fade="true" :imgStyle="{minWidth: '100%'}" />
+    <le-view :src="imgSrc" :defaultImg="defaultImg" :width="200" :height="200" shape="round" :fade="true" :imgStyle="{minWidth: '100%'}" />
   </div>
 </template>
 <script>
     export default {
       data () {
         return {
-          imgSrc: 'https://cdn.leoao.com/o_1blil75gs1p2u1kpm5pp1ov51rfq8.png',
+          imgSrc: '',
           defaultImg: 'https://cdn.leoao.com/m/img/blank_class.png?imageView2/2/w/200'
         }
       },
