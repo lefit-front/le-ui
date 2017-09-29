de3e0eb570577673da566ec432999b0039eff77d
yinxueyuan <838476901@qq.com>
2017-09-29 11:21:45 +0800

    temp: update version

--- examples/pages/view.vue
+++ examples/pages/view.vue
@@ -7,7 +7,7 @@
     export default {
       data () {
         return {
-          imgSrc: '',
+          imgSrc: 'https://cdn.leoao.com/o_1blil75gs1p2u1kpm5pp1ov51rfq8.png',
           defaultImg: 'https://cdn.leoao.com/m/img/blank_class.png?imageView2/2/w/200'
         }
       },
