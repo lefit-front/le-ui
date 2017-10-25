93916921e44488f91813ea561fe48ac855c297a9
yinxueyuan <838476901@qq.com>
2017-10-25 20:06:04 +0800

    fix: checkbox-group component add key param

--- src/components/checkbox/src/checkbox-group.vue
+++ src/components/checkbox/src/checkbox-group.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="le-checkbox-group" :class="vertical ? 'le-checkbox-group-vertical' : 'le-checkbox-group-horizontal'">
-    <Checkbox v-for="(option, index) in options" :label="option.label" :value="option.value" :checked="hasValue(option.value)" @input="changeValue" group :vertical="vertical" />
+    <Checkbox v-for="(option, index) in options" :key="index" :label="option.label" :value="option.value" :checked="hasValue(option.value)" @input="changeValue" group :vertical="vertical" />
   </div>
 </template>
 
