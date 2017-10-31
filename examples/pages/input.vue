bab6c6b4859b3873ff45bcb68309d96c6e948ee7
yinxueyuan <838476901@qq.com>
2017-10-31 20:02:32 +0800

    test: add simple event example for input component

--- examples/pages/input.vue
+++ examples/pages/input.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="">
-    <le-input value="12" placeholder="hello" />
+    <le-input value="12" placeholder="hello" @on-enter="handleEnter"/>
   </div>
 </template>
 <script>
     export default {
+      data () {
+        return {
+        }
+      },
+      methods: {
+        handleEnter () {
+          console.log('enter')
+        }
+      }
     }
 </script>
