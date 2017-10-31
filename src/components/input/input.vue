ebb4c64183c9d651eb67d944ba896c3efbb016d3
yinxueyuan <838476901@qq.com>
2017-10-31 19:19:45 +0800

    feat: init input component file

---
+++ src/components/input/input.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="classes">
+    <Icon :type="icon" :size="iconSize" :color="iconColor">
+  </div>
+</template>
+<script>
+import Icon from '../icon'
+
+const prefixCls = 'le-input';
+export default {
+  name: 'le-input',
+  props: {
+    type: {
+
+    }
+    icon: {
+      type: String
+    },
+    iconSize: {
+      type: [Number, String]
+    },
+    iconColor: {
+      type: String,
+      default: ''
+    }
+  },
+  data: {
+    prefixCls: prefixCls
+  }
+  computed: {
+    classes () {
+      return `${prefixCls} ${prefixCls}-${this.type}`;
+    },
+    styles () {
+      let style = {};
+
+      if (this.size) {
+        style['font-size'] = `${this.size}px`;
+      }
+
+      if (this.color) {
+        style.color = this.color;
+      }
+
+      return style;
+    }
+  }
+};
+</script>
---
+++ src/config/components/input.js
@@ -0,0 +1,33 @@
+export default {
+  file: 'icon',
+  name: 'Icon',
+  class: 'le-icon',
+  describe: '图片容器组件',
+  props: [
+    {
+      name: 'type',
+      type: ['String'],
+      describe: '传入icon图标类名，详见 http://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=174634&keyword=',
+      default: '',
+      option: [],
+      required: true,
+      example: 'logo_lefit'
+    },
+    {
+      name: 'size',
+      type: ['Number', 'String'],
+      describe: 'icon尺寸大小',
+      default: 12,
+      option: [],
+      required: false
+    },
+    {
+      name: 'color',
+      type: ['String'],
+      describe: 'icon的颜色',
+      default: '',
+      option: [],
+      required: false
+    }
+  ]
+}
