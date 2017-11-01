b7691819a462783fab4ae71228e278e87aefc083
yinxueyuan <838476901@qq.com>
2017-11-01 19:52:53 +0800

    feat: add infinite-scroll example

---
+++ examples/pages/infinite-scroll.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul
+    v-infinite-scroll="loadMore"
+    infinite-scroll-disabled="loading"
+    infinite-scroll-distance="10">
+    <li v-for="item in list">{{ item }}</li>
+  </ul>
+</template>
+<script>
+    export default {
+      methods: {
+        loadMore () {
+          this.loading = true;
+          setTimeout(() => {
+            let last = this.list[this.list.length - 1];
+            for (let i = 1; i <= 10; i++) {
+              this.list.push(last + i);
+            }
+            this.loading = false;
+          }, 2500);
+        }
+      }
+    }
+</script>
