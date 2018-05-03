fe5978256cbd6e952e9ad4f1e435a0cae64b6a5e
yinxueyuan <838476901@qq.com>
2018-05-03 15:29:35 +0800

    fix: progress component height auto input

--- src/components/progress/progress.vue
+++ src/components/progress/progress.vue
@@ -2,7 +2,7 @@
   <div class="le-progress">
     <slot name="start"></slot>
     <div class="le-progress-content">
-      <div class="le-progress-runway" :style="{ height: height + 'px' }"></div>
+      <div class="le-progress-runway" :style="{ height: realHeight }"></div>
       <div class="le-progress-progress" :style="{ width: percentage + '%', height: height + 'px' }">
         <div class="le-progress-line" :class="{'le-progress-animation': animation}" :style="{ background: endColor ? `linear-gradient(to right, ${color} 0%, ${endColor} 100%)` : color }"></div>
       </div>
@@ -31,6 +31,14 @@ export default {
       type: Boolean,
       default: false
     }
+  },
+  data () {
+    return {
+      realHeight: ''
+    }
+  }
+  mounted: function () {
+    this.realHeight = typeof(this.height) === 'number' ? this.height + 'px' : this.height
   }
 };
 </script>
