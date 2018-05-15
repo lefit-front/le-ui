264c33b8285ae9042f829bbf60dea8463508b932
yinxueyuan <838476901@qq.com>
2018-05-15 10:37:38 +0800

    fix: fix bug

--- src/components/message-box/src/message-box.vue
+++ src/components/message-box/src/message-box.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="le-msgbox-btns">
           <LeButton :class="[ cancelButtonClasses ]" type="plain" v-show="showCancelButton" @click="handleAction('cancel')" shape="circle">{{ cancelButtonText }}</LeButton>
-          <LeButton :class="[ confirmButtonClasses ]" :size="showCancelButton ? 'normal' : 'large'" v-show="showConfirmButton" @click="handleAction('confirm')" shape="circle" background="inputBackground" color="inputColor">{{ confirmButtonText }}</LeButton>
+          <LeButton :class="[ confirmButtonClasses ]" :size="showCancelButton ? 'normal' : 'large'" v-show="showConfirmButton" @click="handleAction('confirm')" shape="circle" :background="inputBackground" :color="inputColor">{{ confirmButtonText }}</LeButton>
         </div>
       </div>
     </transition>
